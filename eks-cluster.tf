edcff5e58b29924af3fd45b5e42f3d358d89d05a
Tech Mahato | Arbind <135332150+techmahato-com@users.noreply.github.com>
2025-02-05 21:25:11 +0530

    Create eks-cluster.tf

---
+++ eks-cluster.tf
@@ -0,0 +1,15 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "demo-cluster"
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    node_group = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+      instance_types = ["t3.medium"]
+    }
+  }
